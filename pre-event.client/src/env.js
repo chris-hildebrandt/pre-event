1394db571a42987931c04231b76509b35e3589e1
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-23 17:11:41 -0600

    init

--- pre-event.client/src/env.js
+++ pre-event.client/src/env.js
@@ -1,6 +1,12 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'chris-hildebrandt.us.auth0.com'
+export const audience = 'https://codeworks.com'
+export const clientId = 'wZATBDOHloJbDnriV9bpEWd7zw76vSqr'
+
+
+
+
+
+
